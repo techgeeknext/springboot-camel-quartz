88520a28feb96888c9874bdd432979fcf83d599e
TechGeekNeXt <techgeeknext@gmail.com>
2021-07-27 18:48:53 +0530

    TechGeekNext

--- src/main/java/com/techgeeknext/QuartzSchedulerRouteBuilder.java
+++ src/main/java/com/techgeeknext/QuartzSchedulerRouteBuilder.java
@@ -25,13 +25,13 @@ public class QuartzSchedulerRouteBuilder extends RouteBuilder {
         //repeatCount: How many times should the timer repeat?
         //repeatInterval: The amount of time in milliseconds between repeated triggers.
         from("quartz2://currentTimer?trigger.repeatInterval=1000&trigger.repeatCount=5")
-                .setBody().simple("TechGeekNext Quarts Example 1")
+                .setBody().simple("TechGeekNext Quartz Example 1")
                 .to("stream:out");
 
 
         //CASE3 - Print output at console on every 20 minutes
 		/*from("quartz2://Custom?cron=0/20+*+*+?+*+*")
-				.setBody().simple("TechGeekNext Quarts Example 3")
+				.setBody().simple("TechGeekNext Quartz Example 3")
 				.to("stream:out");*/
 
     }
